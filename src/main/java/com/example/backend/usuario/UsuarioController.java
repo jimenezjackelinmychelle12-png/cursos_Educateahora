9e133affc8e779047e9e7f330d7bfe2d10d7164c
jackej <jimenezjackelinmychelle12@gmail.com>
2026-03-24 21:36:38 -0400

    feat: pom y usuario

---
+++ src/main/java/com/example/backend/usuario/UsuarioController.java
@@ -0,0 +1,42 @@
+package com.example.backend.usuario;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+    
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    
+    }
+
+    // GET -> listar usuarios como DTO (sin contraseña)
+    @GetMapping
+    public List<UsuarioDto> listar() {
+        return usuarioService.listarUsuarios();
+    }
+
+    @PostMapping
+    public UsuarioEntity guardar(@RequestBody UsuarioEntity usuario) {
+        return usuarioService.guardar(usuario);
+    }
+
+
+}
