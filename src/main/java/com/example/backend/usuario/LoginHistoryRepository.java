8f9da2008fd232ad52b3e073d3b134a398a05e07
jackej <jimenezjackelinmychelle12@gmail.com>
2026-03-24 22:17:25 -0400

    Delete src/main/java/com/example/backend/usuario directory

--- src/main/java/com/example/backend/usuario/LoginHistoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.usuario;
-
-
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginHistoryRepository extends JpaRepository<LoginHistoryEntity, Long> {
-    List<LoginHistoryEntity> findByUser(UsuarioEntity user);
-}
